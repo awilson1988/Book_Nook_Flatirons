0640395813eb3fcf5ac13b260cd999c24b19551e
amanda <amanda.blaze1988@gmail.com>
2021-03-09 16:49:48 -0800

    Created migrations in DB

--- db/migrations/20210309075814_alter_column_name.rb
+++
@@ -1,5 +0,0 @@
-class AlterColumnName < ActiveRecord::Migration[5.2]
-    def change
-      rename_column :reviews, :comments, :remarks
-    end
-  end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_03_10_004001) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "auhtor"
+    t.string "genre"
+    t.text "summary"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.text "comments"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+  end
 
 end
