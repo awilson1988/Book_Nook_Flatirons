3388995ee9a89cb4f99bd502de4f67921945b153
amanda <amanda.blaze1988@gmail.com>
2021-03-15 19:16:06 -0700

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   get '/books/:id' do
     @book = Book.find_by_id(params[:id])
     @review = @book.reviews
-    
+    puts "Hello"
     erb :'/books/show'
   end
   
