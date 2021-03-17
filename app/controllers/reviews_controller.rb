d056a5872243d8caf432a67faf101dfdf684b98e
amanda <amanda.blaze1988@gmail.com>
2021-03-16 22:48:53 -0700

    Added errors

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,24 +13,26 @@ end
 
     #render form to edit review
     get '/reviews/:id/edit' do
+      redirect_if_not_logged_in
+      @book = Book.find_by(id: params[:book_id])
       @review = Review.find_by(id:params[:id])
       if @review.user == current_user
         erb :'/reviews/edit'
       else 
         flash[:error] = "You can't make changes to that! It doesn't belong to you!"
-        redirect "/books/#{@book.id}"
+        redirect "/books"
     end
   end
     #updates review
     patch '/reviews/:id' do
-      book = Book.find_by(id:params[:id])
+      @book = Book.find_by(id:params[:id])
       @review = Review.find_by(id:params[:id])
       if @review.user == current_user
-      review.update(comments: params[:comments])
-      redirect "/users/#{user.id}" 
+      @review.update(comments: params[:comments])
+        redirect "/books" 
       else 
         flash[:error] = "You can't make changes to that! It doesn't belong to you!"
-        redirect "/books/#{@book.id}"
+        redirect "/books"
       end 
 
 
